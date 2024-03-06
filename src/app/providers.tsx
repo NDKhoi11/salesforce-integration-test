a2772dfb902ac4455efa8051ca0a68a1102f6a36
khoi.nguyen <dangkhoiggg@gmail.com>
2024-03-06 10:08:40 +0700

    update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { queryClient } from "@/utils/react-query";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import { useState } from "react";
 
 type Props = {
   children?: React.ReactNode;
@@ -11,6 +11,17 @@ type Props = {
 };
 
 export const Providers = ({ children, session }: Props) => {
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+        },
+      },
+    })
+  );
+
   return (
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
